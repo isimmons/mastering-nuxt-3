c31ddf0b24a04afb827865791d5df958331bbc1b
isimmons33 <isimmons33@gmail.com>
2022-10-29 20:57:22 -0400

    end of section 1 ready to netlify

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,20 @@
+<template>
+  <iframe
+      width="560"
+      height="315"
+      :src="`https://player.vimeo.com/video/${props.videoId}`"
+      title="Video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+      class="border-none"
+  ></iframe>
+</template>
+
+<script setup>
+const props = defineProps({
+  videoId: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
