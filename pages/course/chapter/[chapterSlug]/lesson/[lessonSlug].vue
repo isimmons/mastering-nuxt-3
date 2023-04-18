ee8969ee9da1ba5a2ce5405eab9146ed2bc49c1b
isimmons <isimmons33@gmail.com>
2023-04-18 16:12:57 -0400

    add course data and useCourse composable

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+const course = useCourse();
+const route = useRoute();
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  );
+});
+
+const lesson = computed(() => {
+  return chapter.value?.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
+</script>
+
 <template>
-  <h1>Chapter: {{ $route.params.chapterSlug }}</h1>
-  <h2>Lesson: {{ $route.params.lessonSlug }}</h2>
-  <p>This is a lesson</p>
+  <h2>Lesson</h2>
+  <p>{{ chapter?.title }}</p>
+  <p>{{ lesson?.title }}</p>
 </template>
