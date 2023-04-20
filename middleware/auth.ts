0ac1351edef00374237ba777561c2c73381b0c63
isimmons <isimmons33@gmail.com>
2023-04-20 18:15:18 -0400

    route protection and login redirect

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,8 @@
-export default defineNuxtRouteMiddleware(({ params }, from) => {
-  if (params.chapterSlug === "1-chapter-1") {
+export default defineNuxtRouteMiddleware(({ params, path }, from) => {
+  const user = useSupabaseUser();
+
+  if (user.value || params.chapterSlug === "1-chapter-1") {
     return;
   }
-  return navigateTo("/login");
+  return navigateTo(`/login?redirectTo=${path}`);
 });
