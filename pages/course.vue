debbaf90b264cccf1a15ab8eed4ef9e9e97a8ee6
isimmons <isimmons33@gmail.com>
2023-04-19 21:05:07 -0400

    add splash to course home

--- pages/course.vue
+++ pages/course.vue
@@ -2,7 +2,9 @@
 const { chapters } = useCourse();
 
 const route = useRoute();
-
+const showSplash = computed<boolean>(() => {
+  return route.path === "/course";
+});
 const isActiveLink = (lessonPath: string): boolean => {
   return lessonPath === route.fullPath;
 };
@@ -48,22 +50,30 @@ const resetError = async (error: Ref<Error | null>) => {
       </div>
     </div>
 
-    <div class="prose p-12 bg-white rounded-md w-[65ch]">
-      <NuxtErrorBoundary>
-        <NuxtPage />
-        <template #error="{ error }">
-          <p>Sorry Charlie! The milk's no good!</p>
-          <p>{{ error }}</p>
-          <p>
-            <button
-              class="hover:cursor-pointer bg-gray-500 text-white font-bold py-1 px-3 rounded"
-              @click="resetError(error)"
-            >
-              Reset
-            </button>
-          </p>
-        </template>
-      </NuxtErrorBoundary>
+    <div class="prose p-5 bg-white rounded-md w-[65ch]">
+      <div v-if="showSplash">
+        <h2>Welcome to the course</h2>
+        <div class="rounded-md overflow-hidden">
+          <img src="/splash.webp" alt="vueschool image" class="m-0" />
+        </div>
+      </div>
+      <div v-else>
+        <NuxtErrorBoundary>
+          <NuxtPage />
+          <template #error="{ error }">
+            <p>Sorry Charlie! The milk's no good!</p>
+            <p>{{ error }}</p>
+            <p>
+              <button
+                class="hover:cursor-pointer bg-gray-500 text-white font-bold py-1 px-3 rounded"
+                @click="resetError(error)"
+              >
+                Reset
+              </button>
+            </p>
+          </template>
+        </NuxtErrorBoundary>
+      </div>
     </div>
   </div>
 </template>
