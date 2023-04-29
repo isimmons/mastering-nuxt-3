ff73e6684916203c2b5cd7150ef6262426bc3e8e
isimmons <isimmons33@gmail.com>
2023-04-29 16:52:06 -0400

    implement basic cache

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,14 +1,26 @@
-export default async (chapterSlug: string, lessonSlug: string) => {
-  const { data, error } = await useFetch(
-    `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
-  );
+import { StorageSerializers } from "@vueuse/core";
 
-  if (error.value) {
-    throw createError({
-      ...error.value,
-      statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`,
-    });
+import type { Lesson } from "~/@types";
+
+export default async (chapterSlug: string, lessonSlug: string) => {
+  const url = `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`;
+
+  const lesson = useSessionStorage<Lesson>(url, null, {
+    serializer: StorageSerializers.object,
+  });
+
+  if (!lesson.value) {
+    const { data, error } = await useFetch<Lesson>(url);
+
+    if (error.value) {
+      throw createError({
+        ...error.value,
+        statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`,
+      });
+    }
+
+    lesson.value = data.value;
   }
 
-  return data;
+  return lesson;
 };
