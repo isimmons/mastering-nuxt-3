d8dc63b1e2ee49d1a8b8469bbd43a734d44c4d18
isimmons <isimmons33@gmail.com>
2023-04-29 17:17:17 -0400

    make generic useFetchWithCache composable

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,26 +1,7 @@
-import { StorageSerializers } from "@vueuse/core";
-
 import type { Lesson } from "~/@types";
 
 export default async (chapterSlug: string, lessonSlug: string) => {
-  const url = `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`;
-
-  const lesson = useSessionStorage<Lesson>(url, null, {
-    serializer: StorageSerializers.object,
-  });
-
-  if (!lesson.value) {
-    const { data, error } = await useFetch<Lesson>(url);
-
-    if (error.value) {
-      throw createError({
-        ...error.value,
-        statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`,
-      });
-    }
-
-    lesson.value = data.value;
-  }
-
-  return lesson;
+  return useFetchWithCache<Lesson>(
+    `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
+  );
 };
