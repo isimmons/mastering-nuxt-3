08ad440858ed6238baf63ae30a7a3fe0bdcf0adb
isimmons <isimmons33@gmail.com>
2023-04-30 19:54:47 -0400

    add generic null checking type assert function

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,9 @@
+export function assertNotNull<T>(arg: unknown): arg is T {
+  if (!!arg) return true;
+
+  throw new Error("argument is null or undefined");
+}
+
 export function assertIsTypeString(arg: unknown): asserts arg is string {
   if (!!arg && typeof arg === "string") {
     return;
