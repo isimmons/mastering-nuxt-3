26207b6542b14318731511e7b968d4ec9dc0d1c1
isimmons <isimmons33@gmail.com>
2023-05-02 05:46:57 -0400

    declare image module types

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -2,4 +2,27 @@ declare global {
   type Maybe<T> = T | null | undefined;
 }
 
-export {};
+declare module "*.jpg" {
+  const path: string;
+  export default path;
+}
+
+declare module "*.jpeg" {
+  const path: string;
+  export default path;
+}
+
+declare module "*.gif" {
+  const path: string;
+  export default path;
+}
+
+declare module "*.png" {
+  const path: string;
+  export default path;
+}
+
+declare module "*.webp" {
+  const path: string;
+  export default path;
+}
