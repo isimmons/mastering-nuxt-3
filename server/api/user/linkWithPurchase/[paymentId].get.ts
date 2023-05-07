7ac9147db6799d02b2976c8a05879a0a2966ca68
isimmons <isimmons33@gmail.com>
2023-05-07 16:40:52 -0400

    refactor type guards

--- server/api/user/linkWithPurchase/[paymentId].get.ts
+++ server/api/user/linkWithPurchase/[paymentId].get.ts
@@ -2,9 +2,11 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 import protectRoute from "~/server/utils/protectRoute";
+import { assertHasParam } from "~/utils";
 export default defineEventHandler(async (event) => {
   protectRoute(event);
-  if (!event.context.params) throw new Error("Missing paymentId parameter...");
+
+  assertHasParam(event.context.params, "paymentId");
   const { paymentId } = event.context.params;
 
   const user = event.context.user;
--- utils/index.ts
+++ utils/index.ts
@@ -12,18 +12,41 @@ export function assertIsTypeString(arg: unknown): asserts arg is string {
   }
 }
 
+export function assertHasParam(
+  arg: unknown,
+  paramName: string
+): asserts arg is { [key: string]: string } {
+  if (!(!!arg && typeof arg === "object" && paramName in arg))
+    throw new Error(`object does not contain the parameter "${paramName}"`);
+}
+
+// export function assertIsSlugObject(arg: unknown): asserts arg is {
+//   lessonSlug: string;
+//   chapterSlug: string;
+// } {
+//   if (
+//     !!arg &&
+//     typeof arg === "object" &&
+//     "lessonSlug" in arg &&
+//     "chapterSlug" in arg
+//   ) {
+//     return;
+//   } else {
+//     throw new Error("argument is not of type string");
+//   }
+// }
+
 export function assertIsSlugObject(arg: unknown): asserts arg is {
   lessonSlug: string;
   chapterSlug: string;
 } {
   if (
-    !!arg &&
-    typeof arg === "object" &&
-    "lessonSlug" in arg &&
-    "chapterSlug" in arg
-  ) {
-    return;
-  } else {
+    !(
+      !!arg &&
+      typeof arg === "object" &&
+      "lessonSlug" in arg &&
+      "chapterSlug" in arg
+    )
+  )
     throw new Error("argument is not of type string");
-  }
 }
