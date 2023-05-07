d342891cb1958551675cfa7cbbcc9b8c11bf0c64
isimmons <isimmons33@gmail.com>
2023-05-07 17:17:20 -0400

    add access check endpoint

---
+++ server/api/user/hasAccess.get.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+type User = {
+  email: string;
+};
+
+export default defineEventHandler(async (event) => {
+  const user = event.context.user as User;
+
+  if (!user) {
+    return false;
+  }
+
+  const coursePurchases = await prisma.coursePurchase.findMany({
+    where: {
+      userEmail: user.email,
+      verified: true,
+      // Hard coded course ID
+      courseId: 1,
+    },
+  });
+
+  // This user has purchased the course
+  return coursePurchases.length > 0;
+});
